5dcf247397587977fba88fd00619ebfa73c91a10
Mix <mnixry@users.noreply.github.com>
2021-10-05 12:09:09 +0800

    使用由`obfs.dev`托管的API

--- src/api/http.js
+++ src/api/http.js
@@ -1,43 +1,43 @@
-import axios from 'axios'
+import _axios from "axios";
 
-const baseURL = 'https://hibiapi.getloli.com/'
-
-axios.defaults.baseURL = baseURL
-axios.defaults.timeout = 10000
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+const axios = _axios.create({
+  baseURL: "https://api.obfs.dev/api/",
+  timeout: 12 * 1000,
+  headers: { "Content-Type": "application/json" },
+});
 
 const get = async (url, params) => {
   try {
-    const res = await axios.get(url, { params })
+    const res = await axios.get(url, { params });
 
     return new Promise((resolve, reject) => {
-      let data = res.data
-      if (typeof data === 'object') {
-        resolve(data)
+      let data = res.data;
+      if (typeof data === "object") {
+        resolve(data);
       } else {
-        reject(data)
+        reject(data);
       }
-    })
+    });
   } catch (ex) {
-    console.error(ex)
+    console.error(ex);
   }
-}
+};
 
 const post = async (url, data) => {
   try {
-    const res = await axios.post(url, data).data
+    const res = await axios.post(url, data).data;
 
     return new Promise((resolve, reject) => {
-      let data = res.data
-      if (typeof res === 'object') {
-        resolve(data)
+      let data = res.data;
+      if (typeof res === "object") {
+        resolve(data);
       } else {
-        reject(data)
+        reject(data);
       }
-    })
+    });
   } catch (ex) {
-    console.error(ex)
+    console.error(ex);
   }
-}
+};
 
-export { get, post }
+export { get, post };
